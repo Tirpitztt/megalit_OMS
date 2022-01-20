98344ce191869a3c6132f60e2161e4448b64f5f2
Sergey Tirpitz <siroga.tt@mail.ru>
2022-01-20 14:35:36 +0300

    add discount field to view

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const path = require('path')
 const cors = require('cors')
 const config = require('config')
 const PORT = process.env.PORT||config.get('port');
@@ -11,17 +12,17 @@ const supportRouter=require('./DAL/Routs/support.routs')
 const db = require('./models');
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
+app.use(express.static('client/build'));
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+})
 app.use('/auth',authRouter);
 app.use('/customer',customerRouter);
 app.use('/calc',calcRouter);
 app.use('/order',orderRouter);
 app.use('/support',supportRouter);
-
-
-
 
 db.sequelize.sync().then((req)=>{
     app.listen(PORT,()=>{
